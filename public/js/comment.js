5e6ef310bcbc25f1b9341269d87331dea93f309d
andresilva8624@gmail.com <andresilva8624@gmail.com>
2022-11-07 15:59:35 -0800

    updated layout

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,32 +1,31 @@
-const commentFormHandler = async function(event) {
-    event.preventDefault();
-  
-    const postId = document.querySelector('input[name="post-id"]').value;
-    const body = document.querySelector('textarea[name="comment-body"]').value;
-  
-    if (body) {
-      const response = await fetch('/api/comment', {
-        method: 'POST',
-        body: JSON.stringify({
-          postId,
-          body
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
+const commentFormHandler = async function (event) {
+  event.preventDefault();
 
-      if (response.ok) {
-        document.location.reload();
-      } else {
-        window.location.replace('/error');
+  const postId = document.querySelector('input[name="post-id"]').value;
+  const body = document.querySelector('textarea[name="comment-body"]').value;
+
+  if (body) {
+    const response = await fetch('/api/comment', {
+      method: 'POST',
+      body: JSON.stringify({
+        postId,
+        body
+      }),
+      headers: {
+        'Content-Type': 'application/json'
       }
-  
-      
+    });
+
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      window.location.replace('/error');
     }
-  };
-  
-  document
-    .querySelector('#new-comment-form')
-    .addEventListener('submit', commentFormHandler);
-  
+
+
+  }
+};
+
+document
+  .querySelector('#new-comment-form')
+  .addEventListener('submit', commentFormHandler);
--- public/js/new.js
+++ public/js/new.js
@@ -1,22 +1,21 @@
-const newFormHandler = async function(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('input[name="post-title"]').value;
-    const body = document.querySelector('textarea[name="post-body"]').value;
-  
-    await fetch(`/api/post`, {
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-        body,
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    document.location.replace('/');
-  };
-  
-  document
-    .querySelector('#new-post-form')
-    .addEventListener('submit', newFormHandler);
-  
+const newFormHandler = async function (event) {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const body = document.querySelector('textarea[name="post-body"]').value;
+
+  await fetch('/api/post', {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      body,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  document.location.replace('/');
+};
+
+document
+  .querySelector('#new-post-form')
+  .addEventListener('submit', newFormHandler);
